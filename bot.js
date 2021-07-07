3b2261c4ef4dc2b4954044a4464b10919e47469b
Dev Patel <devpatel05@gmaill.com>
2021-07-06 19:31:10 -0500

    New Prep

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const Discord= require('discord.js')
 const client= new Discord.Client()
 //bot vars
 const greetings=["Hi","Hello","Greetings"]
+const questions=["What was something that you learned today?","What was somthing productive that you did today?","What was your mood throughout the day?"]
 const name=""
 var ifNext=Boolean(false)
 
@@ -27,29 +28,39 @@ function gotMessage(msg){
     }
     //sets users name
     if(msg.content.substring(0,5)=='$Name'){
-        console.log("Name Recored")
+        console.log("Name Recored!")
         console.log("change1")
         const name=msg.content.substring(5,msg.content.length)
         msg.reply(greetings[randomNumberGenerator(greetings.length)]+ " " +name)
     }
     //create a new entry
     if(msg.content === '$Entry'){
-        console.log("Entry Open")
-        msg.reply(greetings[randomNumberGenerator(greetings.length)]+ " " )
+        console.log("Entry Open.")
+        msg.reply(greetings[randomNumberGenerator(greetings.length)]+ " "+ name+" , I hope you had a good day!" )
+        // for(let i=0;i<questions.length;i++){
+        //     client.channels.send(questions[i])
+        //   // msg.replay(questions[i])
+        // }
         ifNext=true
     }
     //Journal Entry
-    if(ifNext){
+    if(ifNext && !(msg.content==='$Entry') && !(msg.author == client.user)){
         console.log("Entry Logged")
         saveToDoc(msg.content)
         ifNext=false
     }
     //Get all past entries
     if(msg.content === '$Report'){
+
         console.log("Report Given")
+        const fs = require('fs');
+        const path = "/Users/dpatel/Desktop/JournalBot/FileJournalV4.txt";
+        const content = fs.readFileSync(path, 'utf-8');
+        message.channel.send(content);
     }
     //customize what you want in your
     if(msg.content === '$Customize'){
+        
         console.log("customized")
     }
 }
@@ -60,16 +71,13 @@ function randomNumberGenerator(x){
 }
 
 function saveToDoc(txt){
-    // var blob = new Blob([txt],{ type: "text/plain;charset=utf-8" })
-    // globalThis.Blob = Blob;
-    // saveAs(blob, Name+" Journal.txt");
     const fs = require('fs');
 
-fs.writeFile("Desktop/JournalBot/Journals", txt, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-}); 
+    fs.writeFile("FileJournalV6", txt, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    }); 
 
 }
