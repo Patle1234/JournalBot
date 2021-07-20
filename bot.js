4041cb027d9e995cfb71dcbd1947ba642d8f4a67
Dev Patel <devpatel05@gmaill.com>
2021-07-20 00:42:25 -0500

    Added Stuff

--- bot.js
+++ bot.js
@@ -3,9 +3,11 @@ const Discord= require('discord.js')
 const client= new Discord.Client()
 //bot vars
 const greetings=["Hi","Hello","Greetings"]
-const questions=["What was something that you learned today?","What was somthing productive that you did today?","What was your mood throughout the day?"]
+const questions=[]
 const name=""
-var ifNext=Boolean(false)
+var ifNextEntry=Boolean(false)
+var ifNextCustom=Boolean(false)
+var ifFirstEntry=Boolean(true)
 
 //Logging the bot onto the discord server
 client.login('ODU2MDQyNzk3NTkzMDAyMDE0.YM7R-w.9qcZTjTaDiXBXqbkcqKx8u18va8')
@@ -21,47 +23,57 @@ client.on('message', gotMessage);
 function gotMessage(msg){
     console.log(" ")
     //new user
-    if(msg.content ==='$New'){
+    if(msg.content ==='!New'){
         console.log("New User")
         msg.reply("Welcome! My name is JournalBot. What is your name?")
-        msg.reply("Respond with '$NameYOURNAME'")
+        msg.reply("Respond with '!NameYOURNAME'")
     }
     //sets users name
-    if(msg.content.substring(0,5)=='$Name'){
+    if(msg.content.substring(0,5)=='!Name'){
         console.log("Name Recored!")
         console.log("change1")
         const name=msg.content.substring(5,msg.content.length)
         msg.reply(greetings[randomNumberGenerator(greetings.length)]+ " " +name)
     }
     //create a new entry
-    if(msg.content === '$Entry'){
+    if(msg.content === '!Entry'){
         console.log("Entry Open.")
         msg.reply(greetings[randomNumberGenerator(greetings.length)]+ " "+ name+" , I hope you had a good day!" )
-        // for(let i=0;i<questions.length;i++){
-        //     client.channels.send(questions[i])
-        //   // msg.replay(questions[i])
-        // }
-        ifNext=true
+        for(let i=0;i<questions.length;i++){
+            //client.channels.send(questions[i])
+            msg.reply(questions[i])
+        }
+        ifNextEntry=true
     }
     //Journal Entry
-    if(ifNext && !(msg.content==='$Entry') && !(msg.author == client.user)){
+    if(ifNextEntry && !(msg.content==='!Entry') && !(msg.author == client.user)){
         console.log("Entry Logged")
-        saveToDoc(msg.content)
-        ifNext=false
+        if(ifFirstEntry){
+            saveToDoc(msg.content)
+            ifFirstEntry=false
+        }else{
+            editDoc(msg.content)
+        }
+        ifNextEntry=false
     }
     //Get all past entries
-    if(msg.content === '$Report'){
-
+    if(msg.content === '!Report'){
         console.log("Report Given")
-        const fs = require('fs');
-        const path = "/Users/dpatel/Desktop/JournalBot/FileJournalV4.txt";
-        const content = fs.readFileSync(path, 'utf-8');
-        message.channel.send(content);
+        msg.channel.send('Hey there', {
+            files: ['./FileJournalV7.txt']
+          })
+          .catch(console.error);
     }
     //customize what you want in your
-    if(msg.content === '$Customize'){
-        
-        console.log("customizeds")
+    if(msg.content === '!Customize'){
+        console.log("customized")
+        msg.reply("send me the question you want me to ask you")
+        ifNextCustom=true
+    }
+    if(ifNextCustom && !(msg.content==='!Customize' ) && !(msg.author == client.user)){
+        questions.push(msg.content)
+        msg.reply("to add another question/prompt, use !Customize")
+        ifNextCustom =false
     }
 }
 
@@ -70,14 +82,31 @@ function randomNumberGenerator(x){
     return Math.floor(Math.random() * (x))
 }
 
+//creates and saves a journal to a text file
 function saveToDoc(txt){
+    var os = require("os");
     const fs = require('fs');
 
-    fs.writeFile("FileJournalV6", txt, function(err) {
+    fs.writeFile("FileJournalV7.txt", "Entry: "+os.EOL+txt+os.EOL, function(err) {
         if(err) {
             return console.log(err);
         }
         console.log("The file was saved!");
     }); 
 
+}
+
+//edits and saves journal to an existing textfile
+function editDoc(txt){
+    var os = require("os");
+    var newData= "/n"+txt
+    var fs = require('fs')
+    var dataLogger = fs.createWriteStream('FileJournalV7.txt', {
+      flags: 'a' // 'a' means appending (old data will be preserved)
+    })
+    dataLogger.write(os.EOL)//creates a new line
+    dataLogger.write("Entry:")
+    dataLogger.write(os.EOL)
+    dataLogger.write(txt) // append string to your file
+    dataLogger.write(os.EOL)
 }
